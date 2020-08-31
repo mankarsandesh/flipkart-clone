3e1a01fddd602d6b7a1d14bcc5718293fbf88d72
Sandesh <mankarsandesh11@gmail.com>
2020-08-31 08:23:46 +0700

    Working On Footer

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { Component } from "react";
+import { Grid, Card, Box, makeStyles } from "@material-ui/core";
+import './Footer.css';
 
-function Footer() {
-    return (
-        <div>
-            Footer
-        </div>
-    )
+export default function Footer() {
+
+
+  return (
+    <Grid container m={2}>
+      <Grid item xs={12}>
+        <Card>
+          <Box p={2}>
+            <h5>
+              <span className="heading">Top Stories: </span> Brand Directory
+            </h5>
+            <p className="headerWrapper">       
+            <a className="heading">MOST SEARCH FOR ON FLIPKART</a>     
+                <div>
+                <a href="">Flipkart GIft Card Store |</a>
+                </div>
+                  
+                <div>
+                <a href="">Flipkart GIft Card Store |</a>
+                </div>
+            </p>
+          </Box>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 }
-
-export default Footer
